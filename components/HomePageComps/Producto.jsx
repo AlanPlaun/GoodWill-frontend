232621dbcb9b8a40a6e2f46d8f768e6ca58401ea
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-22 11:34:03 -0300

    nose

--- components/HomePageComps/Producto.jsx
+++ components/HomePageComps/Producto.jsx
@@ -20,11 +20,11 @@ const Producto = () => {
     <View style={styles.container}>
       <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
         { listaProductos.map(productos => (
+          
       <View style={styles.card}>
         <CardProducto
         key = {productos.idPublicacion}  nombre = {productos.titulo}  descripcion = {productos.descripcion}  ubicacion = {productos.ubicacion}  nombreUsuario= {productos.nombreUsuario}  imagenUsuario={productos.img} imagenPublicacion={productos.imagen}
         /> 
-        {/* publicador = {productos.nombreUsuario} imgProducto = {productos.imagen} imgPublicador ={productos.img} */}
       </View>
         ))
         } 
